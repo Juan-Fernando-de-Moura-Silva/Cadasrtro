0f14c71ec91a963462bba98c300aa78327988069
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-12 11:40:16 -0300

    ok funcionando

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 function App() {
   const [password, setPassword] = useState("");
   const [title, setTitle] = useState("");
   const [passwordList, setPasswordList] = useState([]);
 
+  useEffect(() => {
+    Axios.get("http://localhost:3001/showpasswords").then((response) => {
+      setPasswordList(response.data);
+    });
+  }, []);
+
   const addPassword = () => {
-    axios.post("http://localhost:3001/addpassword", {
+    Axios.post("http://localhost:3001/addpassword", {
       password: password,
       title: title,
-    }).then(() => {
-      // Após adicionar a senha, você pode atualizar a lista de senhas
-      fetchPasswordList();
     });
   };
 
-  const fetchPasswordList = () => {
-    axios.get("http://localhost:3001/showpasswords")
-      .then((response) => {
-        setPasswordList(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching password list:", error);
-      });
+  const decryptPassword = (encryption) => {
+    Axios.post("http://localhost:3001/decryptpassword", {
+      password: encryption.password,
+      iv: encryption.iv,
+    }).then((response) => {
+      setPasswordList(
+        passwordList.map((val) => {
+          return val.id == encryption.id
+            ? {
+                id: val.id,
+                password: val.password,
+                title: response.data,
+                iv: val.iv,
+              }
+            : val;
+        })
+      );
+    });
   };
-
-  useEffect(() => {
-    fetchPasswordList();
-  }, []);
 
   return (
     <div className="App">
       <div className="AddingPassword">
         <input
           type="text"
-          placeholder="Senha"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
+          placeholder="Ex. password123"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
         <input
           type="text"
-          placeholder="Local"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Ex. Facebook"
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
         />
-        <button onClick={addPassword}>Add Password</button>
+        <button onClick={addPassword}> Add Password</button>
       </div>
 
       <div className="Passwords">
-        {passwordList.map((val, key) => (
-          <div className="password" key={key}>
-            <h3>{val.title}</h3>
-          </div>
-        ))}
+        {passwordList.map((val, key) => {
+          return (
+            <div
+              className="password"
+              onClick={() => {
+                decryptPassword({
+                  password: val.password,
+                  iv: val.iv,
+                  id: val.id,
+                });
+              }}
+              key={key}
+            >
+              <h3>{val.title}</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
